15d8b2d0e38c62fbabafe896547aa1db642b5dbc
LEO <yunusovi747@gmail.com>
2026-04-17 07:51:15 +0900

    feat:python system variables & special methods via dunder

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+# Dunder __ builtins __, __init__
+message = "PYHTON: Everything is object"
+print(message)
+
+result = type(message)
+print("result:", result)
+''' init there are builtin tools
+    (1) TYPES => int float str list dict
+    (2)FUNCTION => print() len() input() type()  int( )
+    (3) CONSTANTS => true false none 
+    '''
+print(dir(__builtins__))
