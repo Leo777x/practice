0c08c7acb731e5d7ec04dad2924471a62cca5d07
LEO <yunusovi747@gmail.com>
2026-04-18 18:02:04 +0900

    faet: add countDigits function

--- train.js
+++ train.js
@@ -1,3 +1,30 @@
+/* ⭐️ B-TASK (Nodejs)
+
+Shunday function tuzing, u 1ta string parametrga ega bolsin,
+ hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+*/
+
+// 🌟 MASALANING YECHIMI:
+
+function countDigits(str) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] >= '0' && str[i] <= '9') {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+const result = countDigits("ad2a54y79wet0sfgb9");
+console.log("result", result)
+
+
+
+
 /* ⭐️ A-TASK (NodeJS)
 
 Shunday 2 parametrli function tuzing, 
@@ -6,6 +33,8 @@ MASALAN countLetter("e", "engineer") 3ni return qiladi.
 */
 
 // 🌟 MASALANING YECHIMI:
+
+/*
 function countLetter(letter, text) {
   let count = 0;
 
@@ -19,3 +48,5 @@ function countLetter(letter, text) {
 }
 const result = countLetter("e", "engineer");
 console.log(result)
+
+*/
