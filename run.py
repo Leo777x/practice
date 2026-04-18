7572e4c4065581d221d5512ab18a19a17c2ddd85
IZZATILLO <izzatillo@IZZATILLOs-MacBook-Pro.local>
2026-04-18 11:15:53 +0900

    feat:python system variables & special methods via dunders

--- run.py
+++ run.py
@@ -4,9 +4,10 @@ print(message)
 
 result = type(message)
 print("result:", result)
-''' init there are builtin tools
+
+''' Init there are builtin tools
     (1) TYPES => int float str list dict
-    (2)FUNCTION => print() len() input() type()  int( )
+    (2)FUNCTION => print() len(uzunlikni o'lchaydi) input() type()  int( )
     (3) CONSTANTS => true false none 
     '''
 print(dir(__builtins__))
