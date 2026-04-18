f9c8a873bf7a7d1c247fd86ef18adc64fb695a8f
IZZATILLO <izzatillo@IZZATILLOs-MacBook-Pro.local>
2026-04-18 12:00:04 +0900

    feat: Learn string in python

--- primitive.py
+++ primitive.py
@@ -11,3 +11,15 @@ print(f"the count: {count} end type: {count_type}")
 result1 = count.bit_count()  # method
 result2 = count.numerator  # state
 print(result1, result2)
+
+print("======== String  ==========")
+# eng ko'p ishlatiladigon methodlar: upper(), lower(), title(), find(), replace()
+
+course = "AI Python Fullstack"
+result = type(course)
+print(f"the result (1): {result}")
+result = course.title()
+print(f"the result (2): {result}")
+result = course.replace("Fullstack", "MasterClass")
+print(f"the result (3): {result}")
+print(course)
