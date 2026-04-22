a5b9dec389500e84cf450cb92d2bc563c52fea1e
LEO <yunusovi747@gmail.com>
2026-04-22 17:35:52 +0900

    feat: learn conditions and logical operators

--- apercon.py
+++ apercon.py
@@ -31,4 +31,41 @@ print(id(c), id(d), id(e))
 
 print("c is d:", c is d)
 print("c is d:", c is e)
- 
+
+print("======= conditions ========")
+
+x = 15
+if x > 50:
+    print("case: A")
+elif x > 10:
+    print("case: B")
+else:
+    print("case: C")
+
+age = 20
+# person = None
+# if age > 16:
+#     person = "adult"
+# else:
+#     person = "child"
+
+
+# Ternary
+person = "adult" if age > 18 else "minor"
+print("person:", person)
+
+print("-------")
+
+is_student = True
+is_admin = False
+is_guest = True
+is_parant = False 
+
+if not is_student:
+    print("Welcome here, do you want to be student!")
+elif is_admin:
+    print("Please go to this office!")
+elif is_guest or is_parant:
+    print("Waiting room is over there")
+else:
+    print("Other cesec")
