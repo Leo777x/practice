14df13e1bdd6a0801ddb16ac6c673965b55cdd05
LEO <yunusovi747@gmail.com>
2026-04-24 03:07:59 +0900

    feat: practice lambda function

--- lists.py
+++ lists.py
@@ -1,7 +1,8 @@
 '''LIST
 (1) Working with lists
 (2) List methods
-(3) enumarate, map and filter
+(3) Lambda function
+(4) enumarate, map and filter
 '''
 print("======= Working with lists ===========")
 # Java/PHP/NodeJS array => python list
@@ -76,3 +77,29 @@ print("sort reverse:", numbers)
 numbs = [2, 20, 12, 100]
 new_numbs = sorted(numbs)
 print(f"the sorted numbs: {numbs} and new_numbs: {new_numbs}")
+
+
+print("======= Lambda function ===========")
+# lambda is small anonymous function!
+
+
+def calculate(x, y): return x * y
+
+
+result = calculate(3, 5)
+print("result:", result)
+
+people = [
+    ("Robert", 20),
+    ("Steve", 19),
+    ("Joseph", 25),
+    ("Michaet", 30),
+    ("Ali", 40)
+]
+people.sort()
+print("people(1)", people)
+
+# sort by age via lambda
+people.sort(key=lambda person: person[1])
+print("people(2)", people)
+
