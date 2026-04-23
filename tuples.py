71a9061142f11a4eb6831a06366c3f6def769a33
LEO <yunusovi747@gmail.com>
2026-04-24 00:51:46 +0900

    feat: unpacking argument via tuple

--- tuples.py
+++ tuples.py
@@ -22,11 +22,38 @@ print("after fruits", fruits)
 
 
 # Biz hechqachon tuple ni qiymatini o'zgartira olmaymiz
-animals = ("dog", "cat", "fish", "lemon") 
+animals = ("dog", "cat", "fish", "lemon")
 
 print(animals[0])
 # manabu yerda o'zgartirib bo'lmaydi sababi tuple bo'lganligi
-# animals[0] = "bird" 
+# animals[0] = "bird"
 
-  
+
+print("======= Unpacking arguments argumenlarni yoyish ===========")
+
+groups = ["MIT", "FLEXY", "DEVEX", "MG"]
+
+(x, y, *z) = groups
+
+print(f"the x: {x} and y: {y}")
+print("z:", z)  # list
+
+
+# *args > tuple
+def calculate(*args):
+    print("*args:", args)
+    total = 1
+    for x in args:
+        total *= x
+    print(f"the total value: {total}")
+    return total
+
+
+# CALL
+calculate(1, 7, 2, 3)
+print("-----------")
+calculate(0, 2, 300)
+print("-----------")
+calculate(5, 7)
+
 
